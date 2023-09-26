9340e44986db5584389c39521d1fd90031262a80
bernyig <bernardommguerreiro@gmail.com>
2023-09-26 17:56:00 +0200

    changed svgs directory on js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
-// import { displayCuriosity } from './changeCuriosity';
 import { handleForm } from './handleForm';
-// import { getInfo, getForecastFor } from './getData';
+import { displayCuriosity } from './changeCuriosity';
 
 handleForm();
+displayCuriosity();
